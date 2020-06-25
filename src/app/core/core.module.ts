4aee5ecb205dbd4a9f4310ecb9985d868dbf55f8
Ronaldo L. Vieira <ronaldo.vieira@dextra-sw.com>
2020-06-25 16:35:36 -0300

    ajusts componente profile

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidbarComponent } from './sidbar/sidbar.component';
-import {MatButtonModule} from '@angular/material/button';
+import { ProfileComponent } from './sidbar/profile/profile.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, SidbarComponent],
+  declarations: [HeaderComponent, FooterComponent, SidbarComponent, ProfileComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
+    MatGridListModule,
     MatIconModule,
     MatSidenavModule,
-    MatButtonModule
+    MatButtonModule,
   ],
   exports: [HeaderComponent, FooterComponent, SidbarComponent],
 })
