37b69228505fc72e32e9f72160c85cf9bdac1e95
Ronaldo L. Vieira <ronaldo.vieira@dextra-sw.com>
2020-06-26 07:56:37 -0300

    menu items

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,14 +6,18 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDividerModule} from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import {MatRippleModule} from '@angular/material/core';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidbarComponent } from './sidbar/sidbar.component';
 import { ProfileComponent } from './sidbar/profile/profile.component';
+import { MenuItemsComponent } from './sidbar/menu-items/menu-items.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, SidbarComponent, ProfileComponent],
+  declarations: [HeaderComponent, FooterComponent, SidbarComponent, ProfileComponent, MenuItemsComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
@@ -21,6 +25,9 @@ import { ProfileComponent } from './sidbar/profile/profile.component';
     MatIconModule,
     MatSidenavModule,
     MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    MatRippleModule
   ],
   exports: [HeaderComponent, FooterComponent, SidbarComponent],
 })
